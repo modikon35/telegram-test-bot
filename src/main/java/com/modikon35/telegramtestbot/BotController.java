a30d6da548d29575e4a25a8c7e7f87b45176d9f3
modikon35 <modikon35@gmail.com>
2021-02-14 21:01:08 +0300

    * New JAVA version

--- src/main/java/com/modikon35/telegramtestbot/BotController.java
+++ src/main/java/com/modikon35/telegramtestbot/BotController.java
@@ -1,11 +1,11 @@
 package com.modikon35.telegramtestbot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class BotController {
 
     private MyTestBot myTestBot;
@@ -16,6 +16,7 @@ public class BotController {
     }
 
     @RequestMapping("/webhook")
+    @ResponseBody
     public String answer() {
         return myTestBot.getBotUsername();
     }
