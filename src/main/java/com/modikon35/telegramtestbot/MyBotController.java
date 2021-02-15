22a1522dacb419fda2c8a3b974e873c3cfe922fb
modikon35 <modikon35@gmail.com>
2021-02-15 09:59:45 +0300

    * Maybe working webhook

--- src/main/java/com/modikon35/telegramtestbot/MyBotController.java
+++ src/main/java/com/modikon35/telegramtestbot/MyBotController.java
@@ -1,10 +1,8 @@
 package com.modikon35.telegramtestbot;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -19,7 +17,7 @@ public class MyBotController {
         this.myTestBot = myTestBot;
     }
 
-    @RequestMapping("/webhook")
+    @PostMapping("/webhook")
     public void webhook(@RequestBody Update update) throws TelegramApiException {
         myTestBot.execute(myTestBot.onWebhookUpdateReceived(update));
     }
