770a7074f4c685e11c5fff778e2d6fc077a4cea8
modikon35 <modikon35@gmail.com>
2021-02-15 14:49:24 +0300

    * Change to webhook init

--- src/main/java/com/modikon35/telegramtestbot/MyBotController.java
+++ src/main/java/com/modikon35/telegramtestbot/MyBotController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.interfaces.BotApiObject;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
@@ -19,8 +22,11 @@ public class MyBotController {
     }
 
     @PostMapping("/webhook")
-    public String webhook(@RequestBody Update update) {
-        return "TESTSTRING!";
+    public BotApiObject webhook(@RequestBody Update update) {
+        Message message = new Message();
+        message.setText("TEXT");
+        message.setChat(update.getMessage().getChat());
+        return message;
         //myTestBot.onWebhookUpdateReceived(update);
     }
 }
