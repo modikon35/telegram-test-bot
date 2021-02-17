b833f7e2df49aa3360bef99ba48581a13336938e
modikon35 <modikon35@gmail.com>
2021-02-17 22:58:22 +0300

    * Поправил зависимости x5

--- src/main/java/com/modikon35/telegramtestbot/MyTestBot.java
+++ src/main/java/com/modikon35/telegramtestbot/MyTestBot.java
@@ -4,6 +4,7 @@ import com.modikon35.untitled.DictionaryParser;
 import com.modikon35.untitled.TranslationController;
 import com.modikon35.untitled.YandexDictionaryParser;
 import com.modikon35.untitled.translation.DetailedTranslation;
+import com.modikon35.untitled.translation.Meaning;
 import com.modikon35.untitled.translation.Translation;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -42,7 +43,13 @@ public class MyTestBot extends TelegramWebhookBot {
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (DetailedTranslation str : translation.getTranslationResult().getMeanings().get(0).getDetailedTranslations()) {
+        List<Meaning> meanings = translation.getTranslationResult().getMeanings();
+
+        if (meanings.isEmpty()) {
+            return new SendMessage(update.getMessage().getChatId().toString(), "Не удалось найти перевод..");
+        }
+
+        for (DetailedTranslation str : meanings.get(0).getDetailedTranslations()) {
             stringBuilder.append(str.getTranslation()).append('\n');
         }
 
