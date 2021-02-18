2ee9f3efeec00b0ec66d9e8041e700acf7839ef1
modikon35 <modikon35@gmail.com>
2021-02-18 22:24:10 +0300

    * Добавил запись пользователей в БД.

--- src/main/java/com/modikon35/telegramtestbot/repositories/words/WordsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.modikon35.telegramtestbot.repositories.words;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordsRepository extends JpaRepository {
-}
